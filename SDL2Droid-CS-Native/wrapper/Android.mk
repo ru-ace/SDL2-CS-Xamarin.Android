20784dcec8406c44910dd7e75f86c665e8d8f7c1
ace <ace@ezotop.ru>
2019-02-23 08:45:08 +0300

    Change path in SDL2Droid-CS-Native/wrapper/Android.mk to build libs for github.com/ru-ace/SDL2-libs-bundle

--- SDL2Droid-CS-Native/wrapper/Android.mk
+++ SDL2Droid-CS-Native/wrapper/Android.mk
@@ -8,8 +8,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := main
-LOCAL_SRC_FILES := sdl2droid-cs-wrapper.c ../../deps/SDL/src/main/android/SDL_android_main.c
+LOCAL_SRC_FILES := sdl2droid-cs-wrapper.c ../SDL/src/main/android/SDL_android_main.c
 LOCAL_LDLIBS := -llog -landroid -lm -ldl
 LOCAL_SHARED_LIBRARIES := SDL2
-LOCAL_CFLAGS += -Ideps/SDL/include -D_REENTRANT
+LOCAL_CFLAGS += -I../SDL/include -D_REENTRANT
 include $(BUILD_SHARED_LIBRARY)
